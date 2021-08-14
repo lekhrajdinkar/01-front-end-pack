91ed9f58d3ef1cac20ec385a37e4acd856367091
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-08-14 11:38:35 -0700

    Dice game

--- VanillaJS/dom/index.js
+++ VanillaJS/dom/index.js
@@ -12,6 +12,8 @@ let player_b = { index:1, name: 'PLAYER-B', total_score: 0, current_score: 0}
 let players = [player_a, player_b];
 let active_player = players[0];
 let temp_total = 0
+let start=false
+let game_log = []
 
 function restart()
 {
@@ -19,7 +21,9 @@ function restart()
     const sure = confirm('Are you Sure ?')
 
     if(sure){
-        document.querySelector(".dice").innerText = '?';
+        let start=true; game_log = []
+        document.querySelector(".dice").innerText = '';
+        document.querySelector("#action-panel").classList.toggle('hide')
         temp_total = 0;
         players_[0].children[3].innerText = 'Your Turn';
         for(let i=0; i<players.length; i++){
@@ -34,17 +38,23 @@ function restart()
 
 function roll(){
     let dice = Math.floor(Math.random() * 7);
+    while(dice === 0 ) dice = Math.floor(Math.random() * 7);
+    
+    game_log.push({player: active_player.name, dice: dice})
     if (dice === 1){
         active_player.current_score = 0
         active_player.total_score = 0
+        //document.querySelector(".dice").innerText = "BAD LUCK, got 1 !!";
         hold()
     }else{
        
         temp_total = temp_total + dice;
         
         console.log(temp_total)
-        if(temp_total >= 50) {
+        if(temp_total >= 25) {
             alert(active_player.name + ' WON !!')
+            console.log(game_log)
+            document.querySelector("#action-panel").classList.toggle('hide')
             restart()
         }
         else{
@@ -59,8 +69,8 @@ function roll(){
 
 function hold()
 {
-    
     let players_ = document.querySelectorAll(".player-containor");
+    game_log.push({player: active_player.name, action: 'hold'})
     document.querySelector(".dice").innerText = '?';
     active_player.total_score = active_player.total_score + active_player.current_score;
     active_player.current_score = 0;
@@ -77,6 +87,8 @@ function hold()
         players_[1].children[3].innerText = ''
     }
     temp_total = active_player.total_score;
+    players_[0].classList.toggle('active-player-bg')
+    players_[1].classList.toggle('active-player-bg')
 }
 
 function updateScore(){
