dce3bc523d5f0cd4b5410f261638dfac2e56f8c3
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-08-21 12:36:03 -0700

    User filter done with JS

--- VanillaJS/basic/component/user-list/script.js
+++ VanillaJS/basic/component/user-list/script.js
@@ -1,7 +1,64 @@
 'use strict';
+
 console.log('User List Demo');
-let userPromise = fetch('https://randomuser.me/api');
-userPromise.then( d =>  console.log(d))
+
+let listdata = [];
+let allUsers = [];
+let res
+getdata();
+document.querySelector('#filter').addEventListener('keydown', filteruser);
+
+async function getdata(){
+    let res = await fetch('https://randomuser.me/api?results=50');
+    allUsers = listdata = await res.json();
+    console.log(listdata);
+    addUser(allUsers.results);
+} 
+
+function addUser(users){
+    users.forEach(u => {
+        let html = `
+        <li>
+            <img src="${u.picture.medium}" alt="">
+            <div class="user-info">
+                <h4>${u.name.first}</h4>
+                <p>Works for CGC, ${u.location.city}</p>
+            </div>
+        </li>`
+        document.querySelector('#result').insertAdjacentHTML('beforeend',html)
+    });
+    document.querySelector('#result').insertAdjacentHTML('beforeend','<li>loading...</li>')
+    
+}
+
+
+function filteruser(e){
+    console.log(e);
+    let text = e.target.value;
+    if(text === '')  {
+        addUser(allUsers.results);
+        return;
+    }
+
+    document.querySelectorAll('li')
+    .forEach(e=>e.classList.add('hide'));
+
+    const result = listdata.results.filter(u => {
+            return u.name.first.includes(text) 
+            || u.name.last.includes(text)
+            || u.location.city.includes(text)
+    })
+
+    document.querySelectorAll('li')
+    .forEach(e=> e.classList.add('hide'));
+
+    addUser(result);
+    console.log(result);
+}
+
+
+
+
 
 
 
