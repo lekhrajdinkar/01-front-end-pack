7baf8da843f387409412be35dd82c82089af88bd
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-08-28 10:46:39 -0700

    height fix

--- StyleHTML/component/carosal/carosal.js
+++ StyleHTML/component/carosal/carosal.js
@@ -13,12 +13,12 @@ console.log(slides)
 const goToSlide = (slide_num) => {
   console.log(curSlide)
   slides.forEach((s,i) => {
-    //s.children[1].classList.remove('carosal--active')
+    s.classList.remove('carosal--active')
     if( i !== slide_num) {
       s.style.transform = `translateX(${100 * (i - slide_num ) }%) scale(0.8)`
     }
     else{
-      //s.children[1].classList.add('carosal--active')
+      s.classList.add('carosal--active')
       s.style.transform = `translateX(${100 * (i - slide_num ) }%)`;
     }
   }) 
