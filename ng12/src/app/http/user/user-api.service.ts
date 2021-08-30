58738dc78bd8bad987d446216c74d1a7672bc9e0
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-08-30 12:08:05 -0700

    user card

--- ng12/src/app/http/user/user-api.service.ts
+++ ng12/src/app/http/user/user-api.service.ts
@@ -7,17 +7,14 @@ import { Observable } from 'rxjs';
 })
 export class UserApiService 
 {
-  user_url = `https://randomuser.me/api?results=50`;
+  user_url = `https://randomuser.me/api?results=20`;
 
   constructor(private http: HttpClient) { 
     this.getUser$(30);
   }
 
-  getUser$(count: number): Observable<any>{
-    return this.http.get(this.user_url)
-    // .subscribe(
-    //   res => console.log(res)
-    // )};
+  getUser$(count?: number): Observable<any>{
+    return this.http.get(this.user_url);
   }
   
 }
--- ng12/src/app/http/user/user.component.ts
+++ ng12/src/app/http/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UserApiService } from './user-api.service';
+import {pipe} from 'rxjs' ;
 
 @Component({
   selector: 'app-user',
@@ -11,11 +12,17 @@ export class UserComponent implements OnInit
   // @Input() theme_text_color: string = 'black';
   // @Input() theme_bg_color: string = 'whitesmoke';
 
-  user :any[] = []
+  users :any[] = [];
   constructor(private userSrv: UserApiService) { }
   
   ngOnInit(): void {
-    this.userSrv.getUser$
+    this.userSrv.getUser$()
+    //.pipe(map)
+    .subscribe(
+      res=> {
+        this.users = res.results;
+        console.log(this.users)
+      });
   }
 
 }
