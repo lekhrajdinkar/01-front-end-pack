2212617d146aebf365cca31e1186b40ee9172c49
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-09-03 11:23:45 -0700

    mergemap amd Switchmap

--- ng12/src/app/rxjs/rxjs.component.ts
+++ ng12/src/app/rxjs/rxjs.component.ts
@@ -1,6 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { Observable, fromEvent, Subscription, Observer, interval } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map, pluck, throttleTime } from 'rxjs/operators';
+import { Observable, fromEvent, Subscription, Observer, interval, of } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, mergeMap, pluck, switchMap, tap, throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -10,16 +11,32 @@ import { debounceTime, distinctUntilChanged, map, pluck, throttleTime } from 'rx
 export class RxjsComponent implements OnInit, AfterViewInit, OnDestroy
 {
   @ViewChild('btnEventObsr') btn!: ElementRef<any>;
-  @ViewChild('inputEventObsr') input!: ElementRef<any>;
+
+  @ViewChild('inputEventObsr1') input_1!: ElementRef<any>;
+
+  //merge
+  @ViewChild('inputEventObsr21') input_21!: ElementRef<any>;
+  @ViewChild('inputEventObsr22') input_22!: ElementRef<any>;
+
+  //switch
+  @ViewChild('inputEventObsr31') input_31!: ElementRef<any>;
+  @ViewChild('inputEventObsr32') input_32!: ElementRef<any>;
   
   Obs_1_click_evt!: Observable<any> ;
-  Obs_2_input_evt!: Observable<any> ;
+  Obs_1_input_evt!: Observable<any> ;
+
+  Obs_21_input_evt!: Observable<string> ;
+  Obs_22_input_evt!: Observable<string> ;
+
+  Obs_31_input_evt!: Observable<string> ;
+  Obs_32_input_evt!: Observable<string> ;
+
   Obs_3_syn_data_array!: Observable<string> ;
-  Obs_4_interval = interval(1000);
+  Obs_4_interval :Observable<number>  = interval(1000);
 
   subs : Subscription[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void { }
 
@@ -31,11 +48,59 @@ export class RxjsComponent implements OnInit, AfterViewInit, OnDestroy
     this.register_obs1();
     this.register_obs2();
     this.register_obs3();
-    // this.register_obs4();  <<<<< HERE
-    
+    this.register_mergemap_obs();
+    this.register_switchmap_obs();
   }
 
   // =======================Observables============
+  merge_map_data : any[] = [];
+  merge_map_outer_o_data : any[] = [];
+  merge_map_inner_o_data : Set<string> = new Set();
+
+  register_mergemap_obs(){
+    this.Obs_21_input_evt = fromEvent(this.input_21.nativeElement, 'input').pipe(pluck('target','value'));
+    this.Obs_22_input_evt = fromEvent(this.input_22.nativeElement, 'input').pipe(pluck('target','value'));
+
+    // mergemap take cb fn, which must return anothor obs
+    // mergemap keeps all the data emitted by obs and then run cb for each of them
+    // For every data released by outter obs, it will run get/subscribe data from inner obs
+    // returns data emitted by inner obs
+    this.Obs_21_input_evt
+    .pipe( 
+      tap( ood => this.merge_map_outer_o_data.push(ood)),
+      tap( od => console.log(od)),
+      mergeMap( v =>  this.Obs_22_input_evt
+        .pipe(
+          debounceTime(1000),
+          tap( iod => this.merge_map_inner_o_data.add(iod)),
+          map( i => v + " "+i)))
+    )
+    .subscribe( result => this.merge_map_data.push(result));
+
+  }
+
+  switch_map_data : any[] = [];
+  switch_map_outer_o_data : any[] = [];
+  switch_map_inner_o_data : Set<string> = new Set();
+
+  register_switchmap_obs(){
+    this.Obs_31_input_evt = fromEvent(this.input_31.nativeElement, 'input').pipe(pluck('target','value'));
+    this.Obs_32_input_evt = fromEvent(this.input_32.nativeElement, 'input').pipe(pluck('target','value'));
+
+    this.Obs_31_input_evt
+    .pipe( 
+      tap( od => console.log(od)),
+      tap( ood => this.switch_map_outer_o_data.push(ood)),
+      switchMap( v =>  this.Obs_32_input_evt
+        .pipe(
+          debounceTime(1000),
+          tap( ood => this.switch_map_inner_o_data.add(ood)),
+          map( i => v + " "+i)))
+    )
+    .subscribe( result => this.switch_map_data.push(result));
+  }
+
+  obs1_data :any[] = [];
   register_obs1(){
     console.log(this.btn);
     this.Obs_1_click_evt = fromEvent(this.btn.nativeElement, 'click');
@@ -46,17 +111,20 @@ export class RxjsComponent implements OnInit, AfterViewInit, OnDestroy
       map( (evt: PointerEvent) => ({x: evt.clientX, y: evt.clientY}) ),
     )
     .subscribe(
-      (points) => {console.log('Obs_1_click pointer coordinate: ', points)}
+      (points) => {
+        this.obs1_data.push(points);
+        console.log('Obs_1_click pointer coordinate: ', points)}
     )
     this.subs.push(sub1);
   }
 
   // ==============
+  obs2_data :string[] = [];
   register_obs2(){
     console.log(this.btn);
-    this.Obs_2_input_evt = fromEvent(this.input.nativeElement, 'input');
+    this.Obs_1_input_evt = fromEvent(this.input_1.nativeElement, 'input');
 
-    const sub2 = this.Obs_2_input_evt
+    const sub2 = this.Obs_1_input_evt
     .pipe(
      // map(evt => evt.target.value ),
       pluck('target', 'value' ),
@@ -65,7 +133,9 @@ export class RxjsComponent implements OnInit, AfterViewInit, OnDestroy
      // map(evt => evt.target.value ),
     )
     .subscribe(
-      (value: string) => {console.log('>> Input text: ', value)}
+      (value: string) => {
+        this.obs2_data.push(value);
+        console.log('>> Input text: ', value)}
     )
     this.subs.push(sub2);
   }
@@ -87,34 +157,47 @@ export class RxjsComponent implements OnInit, AfterViewInit, OnDestroy
   }
 
   // ==============
+  obs3_obs1_data: string[] = [];
   subscribe_obs3_s1()
   {
     const obsrvr1: Observer<any> = { 
-      next: (value: string) => {console.log('>> Observer--1 : ', value)},
+      next: (value: string) => {
+        this.obs3_obs1_data.push(value);
+        console.log('>> Observer--1 : ', value)
+      },
       error: (err: any) => {},
       complete: () => {}
     } 
-    const sub3 = this.Obs_3_syn_data_array.subscribe(obsrvr1);
+    const sub3 = this.Obs_3_syn_data_array
+    .pipe(debounceTime(500))
+    .subscribe(obsrvr1);
     this.subs.push(sub3);
   }
 
+  obs3_obs2_data: string[] = [];
   subscribe_obs3_s2(){
     const sub3 = this.Obs_3_syn_data_array
     .subscribe(
-      (value: string) => {console.log('>> Observer--2 : ', value)}
+      (value: string) => {
+        this.obs3_obs2_data.push(value);
+        console.log('>> Observer--2 : ', value)
+      }
     )
     this.subs.push(sub3);
   }
 
   // ==============
+  obs4_data: number[] = []
   register_obs4(){
     const sub = this.Obs_4_interval
     .pipe(
-      // throttleTime(2000)
-      //debounceTime(2000),
+      //throttleTime(2000)
+      //debounceTime(100),
     )
     .subscribe(
-      (value: number) => {console.log(' -- interval --  ', value)}
+      (value: number) => {this.obs4_data.push(value);
+      if (value > 10) { sub.unsubscribe()}
+     }
     )
     this.subs.push(sub);
   }
