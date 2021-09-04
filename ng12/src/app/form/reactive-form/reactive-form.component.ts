78f135b8fefc87ba8e03063b54b0e91096051a38
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-09-04 07:57:33 -0700

    Forms 2

--- ng12/src/app/form/reactive-form/reactive-form.component.ts
+++ ng12/src/app/form/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TestabilityRegistry } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormComponent implements OnInit {
 
+  form!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      'fname': new FormControl(null, [Validators.required]),
+      'lname': new FormControl(null, [Validators.required]),
+      'email': new FormControl(null, [Validators.required])
+    })
   }
 
+  // Form Action
+  printForm(){ 
+    console.log(this.form);
+    
+  }
+  patch(){
+    this.form.patchValue({fname:'Lekhraj'})
+    console.log(this.form.get('fname'));
+  }
+  reset(){ 
+    this.form.reset()
+  }
+
+  items=[
+    '1.setValue, patchvalue, reset, getvalue, disable feilds',
+    '2.FormGrup, FormGroupname, FormControlName ',
+    '3.sync and aysn validator, conditionally add or remove validator',
+    '4. FormArray',
+    '5. touched, valid, errormessage and styling'
+  ]
 }
