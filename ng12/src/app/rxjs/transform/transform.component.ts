7b2cbda813564f85745c61c5e57517225022c3d8
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-09-09 11:58:04 -0700

    BufferCount

--- ng12/src/app/rxjs/transform/transform.component.ts
+++ ng12/src/app/rxjs/transform/transform.component.ts
@@ -1,6 +1,6 @@
 import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { fromEvent, interval, Observable } from 'rxjs';
-import { buffer, takeUntil, tap } from 'rxjs/operators';
+import { buffer, bufferCount, take, takeUntil, tap } from 'rxjs/operators';
 
 const p =( d:any)=>console.log(d);
 
@@ -12,21 +12,34 @@ const p =( d:any)=>console.log(d);
 export class TransformComponent implements OnInit, AfterViewInit {
 
   buffer$!: Observable<any>;
+  bufferCount$!: Observable<any>; 
+
   constructor() { }
 
   ngOnInit(): void {}
   @ViewChild('bufferBtn')btn_buffer!: ElementRef<any>;
-
-
+  @ViewChild('bufferCountBtn')btn_bufferCount!: ElementRef<any>;
+  
   ngAfterViewInit(): void {}
 
+  // 1 buffer
   buffer_test(){
     const clicks$ = fromEvent(this.btn_buffer.nativeElement, 'click');
     this.buffer$ = interval(1000).pipe( 
       tap(p)
-      ,takeUntil(clicks$ )
+      ,takeUntil(clicks$ ) //comment me and check
       ,buffer( clicks$ ) 
+       
     )
-}
+  }
+//2
+  bufferCount_test(){
+    const clicks$ = fromEvent(this.btn_bufferCount.nativeElement, 'click');
+    this.bufferCount$ = interval(1000).pipe( 
+      tap(p)
+      ,takeUntil(clicks$ )
+      ,bufferCount( 5 ) 
+    )
+  }
   
 }
