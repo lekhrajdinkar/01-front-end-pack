894cf055d7d224fec2c7a9f68bfcbd0ef7f49092
lekhrajdinkar <lekhrajdinkar@gmail.com>
2025-06-10 16:20:22 -0700

    TS

--- SupersetTS/basic/01_intro.md
+++ SupersetTS/basic/01_intro.md
@@ -1,20 +1,81 @@
 # TypeScript
-
-- Cheatsheet-1 : https://chat.deepseek.com/a/chat/s/02a193f8-5082-4aad-a41c-4e9f5e16af4b
-
-## intro
-
-- language plus compiler to transcompile
-- has no runtime
+- datatype : https://chat.deepseek.com/a/chat/s/02a193f8-5082-4aad-a41c-4e9f5e16af4b
 
 ---
+## intro
+- language plus compiler to transpile
+- has no runtime
+- tsc compilter option
+```
+--strict                Enable all strict type checking options
+--noImplicitAny         Raise error on expressions with implied 'any' type
+--strictNullChecks      Enable strict null checks
+--target es5 | es6      Specify ECMAScript target version
+--module commonjs       Specify module code generation
+--outDir ./dist         Redirect output structure to directory
+```
 
+---
 ## Adv of TS:
-
-- Class, interface, generic, Access specifies
-- Add type in function arg, return type.
-- obj1.proNotExist = "someValue" // cannot do in TS
-- prevents hetrogeneous array, has another type Tuple. Also Enum.
-- adds additinal feature to evoling ES6+ (access specifier, interface, generics, enums) syntax wise by using JS workarounds. JS might have complex snippets to perform same task.
-- Strong typing - Make JS from Dynamic type to static type
+- adds additional feature to evolving ES6+
+- Make JS from Dynamic type to static type
 - Catch lots of error at compile time, makes JS robust, can write better code.
+
+### OOPS: Class, interface, Enum, generic, Access specifies
+- obj1.proNotExist = "someValue" // cannot do in TS
+- no more `object literal`, use class template
+- no more `prototype`
+- no more `constructor function`
+- fact : just syntactical sugar
+
+### String typing (new things)
+- [007_JS_ES6.md](../../VanillaJS/NOTES_JS/007_JS_ES6.md)
+- **symbol** from ES6+
+- **never**
+- **tuple**
+- **Type aliases**
+  - type mytype = "abc" | "xyx"
+  - type mytype = String | number  // OR, UNION
+  - type mytype = Partial<User>
+  - type mytype = Partial<string>
+  - type mytype = **keyof** Class1  // "key1" | "key2" | "key3" | ...
+
+```typescript
+// &, INTERSECTION
+
+interface A { a: number }
+interface B { b: string }
+type AB = A & B; // { a: number, b: string } 
+```
+- **Utility Types** :point_left:
+  - type mytype = `Partial<T>` / `Required<T>`  : all properties of T to **optional /required**
+  - type mytype = `Readonly<T>` : all properties of Type set to **readonly**.
+  - type mytype = `Pick<T> / omit<T>`
+  - type mytype = `Record<K,T>`
+  - type mytype = `ReturnType<() => string>`
+  - type mytype = `NonNullable<T>`
+  - ...
+  - so basically, source type : T,K || target type : mytype
+  - mpre: https://chat.deepseek.com/a/chat/s/3c19b702-9118-49ed-9599-35975a1fbef5
+  
+- **Index signature** :point_left:
+```typescript
+interface MyObject {
+  [propName: string]: any; // Index signature
+}
+
+const obj: MyObject = {
+  name: "Alice", // OK (string)
+  age: 30,       // OK (number)
+  isAdmin: true, // OK (boolean)
+  someMethod() { return 42; } // OK (function)
+};
+
+// ----
+interface MyObject {
+  [propName: Class1]: any; // INVALID : can only have keys of type string, number, or symbol
+}
+```
+
+
+
--- SupersetTS/basic/data.type.ts
+++ SupersetTS/basic/data.type.ts
@@ -1,10 +1,8 @@
-// The core primitive types in TypeScript are all lowercase!
-// its number and string... Not String and Number
-// Javascript already know abt by using TypeOf operator, but its avialbale at runtime.
-// so explicitly we have to additional validation code for type which will executed at runtime.
-// Type script provides type check at compile time. igt adds comilier phase for JS.
+// ==== Type Assertions ====
+let someValue: any = "this is a string";
+let strLength1: number = (<string>someValue).length; // Angle-bracket syntax
+let strLength2: number = (someValue as string).length; // as-syntax
 
-console.log("Hello TypeScripts");
 
 // A. string, number, boolean, BigInt, undefined, null, NaN
 let n3: number;
@@ -13,66 +11,41 @@ let n4 = 23; //  inference type by first assignment
 let n6; //: number;
 n6 = "23";
 
-console.log(typeof n6);
+console.log(typeof n6); // java:instanceOf  py:type()
 
 // B. Object
-//all 4 are same
 const obj0: any = { name: "lekhraj1" }; // IMP: use any means loosing all adv offered by TS for static typing.
 const obj1: {} = { name: "lekhraj1" };
-const obj2: object = { name: "lekhraj2" };
-const obj3 = { name: "lekhraj3" };
-// console.log(obj1.name, obj1.age); //error TS2339: Property 'age' does not exist on type '{ name: string; }'.
-// const obj4: {age: number} = { name: 'lekhraj'} // error
 
 // C. Array and Tuple
 const obj5: any = {
   name: "lekhraj1", //string
   hobbies: ["soccer", "study"], //string[]
-  role: [2, "Dev", { team: "TACT", client: "CGC" }], //tuple
+  role: [2, "Dev", { team: "TACT", client: "CGC" }], //tuple vary size
 };
-obj5.hobbies.push("hiking");
+obj5.hobbies.push("hiking"); //push/pop,slice,splice, shift/unshift
+console.log(obj5);
+obj5.role.push(2012); // can add more on tuple  <<<
 console.log(obj5);
 
-obj5.role.push(2012); // can add more on tuple
-console.log(obj5);
-
-// Fixed size tuple
-// let role: [] = [2, 'Dev', {team: 'TACT', client: 'CGC'}] //tuple
-let role: [number, number, string, any] = [
-  5,
-  2,
-  "Dev",
-  { team: "TACT", client: "CGC" },
-]; //tuple
-
-role.push(2021); //Exception : push is not allowed.
+// FIXED size :: tuple
+let role1: [] = [2, 'Dev', {team: 'TACT', client: 'CGC'}] //tuple vary size
+let role2: [number, number, string, any] = [  5,  2,  "Dev",  { team: "TACT", client: "CGC" }]; //tuple fixed size
+role2.push(2021); //Exception : push is not allowed <<<
 console.log(role);
 
 // D. Enum
-enum Day {
-  MON,
-  TUES,
-}
-enum Role {
-  DEV = 100,
-  TST = 200,
-}
-enum Role2 {
-  DEV = "dev",
-  TST = "test",
-}
+enum Day {  MON,  TUES}
+enum Role {  DEV = 100,  TST = 200}
+enum Role2 {  DEV = "dev",  TST = "test"}
 
-// E. any : Flexible
-// avoid using it.
-// compiler can check.
-// use any, then add some runtime validation
-
-//F. Union Type
-type alias_type = number | String; //  TYPE keyword, makes sense with union
-type alias_type_string_only = String; //is also possible then. no sense. // can be use like interface then.
+//==============================
+//E. Union/intersection Type
+//==============================
+type alias_type = number | String;
+type alias_type_string_only = String;
 
 function combine(n1: alias_type, n2: alias_type) {
-  //function combine(n1: number|String, n2: number|String){
   let result;
   if (typeof n1 === "number" && typeof n2 === "number") {
     console.log(" got number, doing addition");
@@ -89,8 +62,9 @@ combine("30", 40);
 combine(30, "40");
 combine("30", "40");
 
+//=================
 // G. Literal Type
-// very sure about value;
+//=================
 const operation: "add" | "subtract" = "add";
 const operation2: "multiply" | "divide" = "divide";
 let operation3: "expo" | "median" = "expo";
@@ -98,7 +72,9 @@ function operations(o: "add" | "subtract") {}
 operations(operation);
 //operations(operation2); // invalid
 
-// H.Function Type
+//=================
+// H. Function Type
+//=================
 let f1: Function;
 f1 = function () {};
 
@@ -106,26 +82,19 @@ let f2: () => any | {} | object; // function which  can return any, or {} or obj
 f2 = function () {
   // return {}; // ok
   return { p1: "p1", p2: "p2" }; // ok
-};
+}
 
 let f3: (a: string, b: number) => string;
-f3 = function (a1: string, b1: number) {
-  return "";
-};
+f3 = function (a1: string, b1: number) {  return "";}
 
 let f4: (a: string, b: () => void) => string;
-const cb = () => {
-  return null;
-};
-f4 = function (a1: string, cb) {
-  return "";
-};
+f4 = function (a1: string, () => {  return null;}) {  return ""; }
 
+//======================
 // I. Unknown and never
-// unkown more like any
-
-//no usecasse, just for code quality
+//=======================
 function throwerror(): never {
   // not void
   throw { msg: "error occured", code: "400" };
 }
+
